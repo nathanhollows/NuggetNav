79cc1a66cf613b96db858c0e3a085e1dd96d03b9
nathanholows <nathanhollows>
2019-10-25 23:16:40 +1300

    Testing if android studio attributes my commits

--- app/src/main/java/com/nuggetwatch/nuggetnav/MapActivity.java
+++ app/src/main/java/com/nuggetwatch/nuggetnav/MapActivity.java
@@ -238,6 +238,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 iconAllowOverlap(true),
                 iconSize(0.9f)
         );
+        
         loadedMapStyle.addLayer(unclustered);
 
         for (int i = 0; i < layers.length; i++) {
